3a3cdecbc7aa9cf370cab55d64cffa7095fe6317
Vishal260305 <vg260305@gmail.com>
2025-07-09 20:30:23 +0530

    🔐 Shifted to MongoDB Atlas & secured secrets

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI).then(()=>console.log("Connected")).catch((e)=>console.log(e.message))
-
-}
+  mongoose.connect(mongoURI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((e) => console.log(e.message));
+};
 
 module.exports = connectToMongo;
